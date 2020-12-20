e3ee89fdb6873d83fbd1570eff0d65539ce7cade
enrique <enriqueyr123@gmail.com>
2020-12-20 18:57:27 +0100

    Movimiento de jugadores y habilidad dash del atacante

---
+++ Scripts/Players/Girl.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl : Player
+{
+    public int shootsPerSecond=3;
+    public int shootDamage=2;
+    public int health = 100;
+
+    private int nDash = 2;
+    public int nDashDefault = 2;
+
+    private float dashTime = 0;
+    private float defTime = 0;
+
+    public override void Start()
+    {
+        base.Start();
+        cdOf = 1 / shootsPerSecond;
+        cdDef = 0.75f;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        InputsG();
+        checkDash();
+
+    }
+
+    private void checkDash()
+    {
+        if (dash)
+        {
+            dashTime += Time.deltaTime;
+            if (dashTime > 0.3)
+            {
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x / 3f, 0, rigidBody.velocity.z / 3f);
+                dash = false;
+                dashTime = 0;
+            }
+        }
+
+        if(defTime > 0)
+        {
+            defTime += Time.deltaTime;
+            if (defTime > cdDef)
+            {
+                defTime = 0;
+                nDash =nDashDefault;
+            }
+        }
+    }
+
+    private void InputsG()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && nDash>0)
+        {
+            nDash -= 1;
+            Dash();
+        }
+    }
+
+    private void Dash()
+    {
+        if (nDash == 0)
+        {
+            changeDashCd();
+        }
+        else
+        {
+            releaseDashCd();
+        }
+        dash = true;
+        defTime += 0.01f;
+        rigidBody.velocity = new Vector3(rigidBody.velocity.x*3f,0,rigidBody.velocity.z*3f);
+    }
+
+    private void changeDashCd()
+    {
+        cdDef = 3;
+    }
+
+    private void releaseDashCd()
+    {
+        cdDef = 0.75f;
+    }
+
+    private void changeShootsPerSecond(int increment)
+    {
+        shootsPerSecond += increment;
+        cdOf = 1 / shootsPerSecond;
+    }
+}
---
+++ Scripts/Players/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed = 6f;
+    public float cdOf;
+    public float cdDef;
+
+    private Vector2 direction;
+    private float dirX;
+    private float dirY;
+
+    public bool dash=false;
+
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        Inputs();
+    }
+
+
+    public virtual void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        if (!dash)
+        {
+            rigidBody.velocity = new Vector3(direction.x * movementSpeed, 0, direction.y * movementSpeed);
+        }
+    }
+
+    void Inputs()
+    {
+        dirX = Input.GetAxisRaw("Vertical");
+        dirY = Input.GetAxisRaw("Horizontal");
+        direction = new Vector2(dirY, dirX);
+    }
+
+    public Vector2 getLookingDirection()
+    {
+        return direction;
+    }
+}
