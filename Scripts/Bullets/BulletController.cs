3d7fc2c61e40879611bfad6072413bc1feacff17
enrique <enriqueyr123@gmail.com>
2020-12-26 00:25:59 +0100

    Añadido control con raton y rotacion del modelo segun la direccion

--- Scripts/Bullets/BulletController.cs
+++ Scripts/Bullets/BulletController.cs
@@ -19,23 +19,46 @@ public class BulletController : MonoBehaviour
        
     }
 
-    public Vector2 dirCalculate(Vector3 pos)
+    public Vector3 dirCalculate(Vector3 pos)
     {
-        var mousepos = Input.mousePosition;
-        mousepos.z = 16;
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mousepos).normalized;
+        Vector3 target=new Vector3(0,0,0);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Default")))
+        {
+            if (hit.collider.tag.Equals("Floor"))
+            {
+                target = hit.point - transform.position;
+                return target.normalized;
+            }
+        }
 
-        return mousePosition;
+        return target.normalized;
     }
 
     public void Shoot(Vector3 pos, Vector2 dirA)
     {
-        Bullet aux=Instantiate(bullet, pos, Quaternion.identity);
-        //aux.dir = dirCalculate(pos);
-        aux.dir = dirA;
-        aux.shootSpeed=stats.GetStat(Stat.SHOT_SPEED);
-        aux.damage = stats.GetStat(Stat.SHOT_DMG);
-        aux.playerShoot = stats.GetStat(Stat.IS_PLAYER);
-        aux.transform.localScale = new Vector3(stats.GetStat(Stat.SHOT_SIZE), stats.GetStat(Stat.SHOT_SIZE), stats.GetStat(Stat.SHOT_SIZE));
+        if (stats.GetStat(Stat.IS_PLAYER) == 0)
+        {
+            Vector3 dir = dirCalculate(pos);
+            if (dir.x != 0 && dir.y != 0 && dir.z != 0)
+            {
+                Bullet aux = Instantiate(bullet, pos, Quaternion.identity);
+                aux.dir = dirCalculate(pos);
+                aux.shootSpeed = stats.GetStat(Stat.SHOT_SPEED);
+                aux.damage = stats.GetStat(Stat.SHOT_DMG);
+                aux.playerShoot = stats.GetStat(Stat.IS_PLAYER);
+                aux.transform.localScale = new Vector3(stats.GetStat(Stat.SHOT_SIZE), stats.GetStat(Stat.SHOT_SIZE), stats.GetStat(Stat.SHOT_SIZE));
+            }
+        }
+        else
+        {
+            Bullet aux = Instantiate(bullet, pos, Quaternion.identity);
+            aux.dir = dirA;
+            aux.shootSpeed = stats.GetStat(Stat.SHOT_SPEED);
+            aux.damage = stats.GetStat(Stat.SHOT_DMG);
+            aux.playerShoot = stats.GetStat(Stat.IS_PLAYER);
+            aux.transform.localScale = new Vector3(stats.GetStat(Stat.SHOT_SIZE), stats.GetStat(Stat.SHOT_SIZE), stats.GetStat(Stat.SHOT_SIZE));
+        }  
     }
 }
