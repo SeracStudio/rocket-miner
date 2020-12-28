178fe4e8414d96ae1de9fd3c994126343c3b802d
Pablo <pabloinfo2015.16@gmail.com>
2020-12-28 21:25:10 +0100

    ui inicial

---
+++ Scripts/UI/LanguageChanger.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageChanger : MonoBehaviour
+{
+
+    public string TextID;
+    public Text text;
+
+    private string path;
+    private Dictionary<string, string> jsonChanger;
+    public TextAsset language;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        jsonChanger = new Dictionary<string, string>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadJson(TextAsset json)
+    {
+        JSONarray jSONarray = JsonUtility.FromJson<JSONarray>(json.text);
+        foreach(JSONpair jSONpair in jSONarray.Idioma)
+        {
+            jsonChanger.Add(jSONpair.id, jSONpair.content);
+        }
+    }
+
+    public void ChangeJSON()
+    {
+        LoadJson(language);
+        text.text = jsonChanger[TextID];
+    }
+
+
+
+[System.Serializable]
+private class JSONpair
+    {
+        public string id;
+        public string content;
+
+
+    }
+
+    private class JSONarray
+    {
+        public JSONpair[] Idioma;
+    }
+
+
+}
---
+++ Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+    public GameObject MenuToActivate;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RemoveMainMenu()
+    {
+        MainMenu.SetActive(!MainMenu.activeSelf);
+        MenuToActivate.SetActive(!MenuToActivate.activeSelf);
+
+    }
+}
