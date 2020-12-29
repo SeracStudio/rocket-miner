08341d4279ccb6286c69553d024295858a790d74
Pablo <pabloinfo2015.16@gmail.com>
2020-12-29 21:50:40 +0100

    ui inicial done

---
+++ Scripts/UI/CameraMover.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+
+    public Camera camera;
+    public static CameraMover RUNNING;
+
+
+    private float startTime;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        RUNNING = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveCameraTo(Vector3 pos)
+    {
+        startTime = Time.time;
+        StartCoroutine("MoveAnimation", pos);
+    }
+
+    IEnumerator MoveAnimation(Vector3 pos)
+    {
+        Vector3 initialPos = camera.transform.position;
+        float percentage = 0f;
+        while (percentage < 1f)
+        {
+            percentage = (Time.time - startTime) / 1.0f;
+            camera.transform.position = Vector3.Lerp(initialPos, pos, percentage);
+            yield return null;
+        }
+    }
+
+    public void SetMenu(GameObject menu, bool status, float timing)
+    {
+        object[] vs = new object[3];
+        vs[0] = menu;
+        vs[1] = status;
+        vs[2] = timing;
+        StartCoroutine("SetMenuCoroutine", vs);
+    }
+
+    IEnumerator SetMenuCoroutine(object[] vs)
+    {
+        GameObject menu = vs[0] as GameObject;
+        bool status = (bool) vs[1];
+        float timing = (float) vs[2];
+        
+        yield return new WaitForSeconds(timing);
+        menu.SetActive(status);
+    }
+}
--- Scripts/UI/MainMenuManager.cs
+++ Scripts/UI/MainMenuManager.cs
@@ -7,23 +7,36 @@ public class MainMenuManager : MonoBehaviour
 
     public GameObject MainMenu;
     public GameObject MenuToActivate;
+    public Camera camera;
+    public Vector3 target;
+    private float speed;
+
+    private float startTime;
+    private Vector3 initialPos;
+    private float distance;
 
     // Start is called before the first frame update
     void Awake()
     {
-
+        startTime = Time.time;
+        initialPos = camera.transform.position;
+        distance = Vector3.Distance(target, initialPos);
+        speed = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {  
+    
     }
 
     public void RemoveMainMenu()
     {
         MainMenu.SetActive(!MainMenu.activeSelf);
-        MenuToActivate.SetActive(!MenuToActivate.activeSelf);
-
+        CameraMover.RUNNING.MoveCameraTo(target);
+        CameraMover.RUNNING.SetMenu(MenuToActivate, true, 1.0f);
+        //MenuToActivate.SetActive(!MenuToActivate.activeSelf);
     }
+
+  
 }
