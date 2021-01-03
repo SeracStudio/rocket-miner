5b088735cb7d6b7408d8202408e722f219a891e9
enrique <enriqueyr123@gmail.com>
2021-01-03 15:54:14 +0100

    Bosses, arreglado bug 2

--- Scripts/Players/Robot.cs
+++ Scripts/Players/Robot.cs
@@ -20,7 +20,6 @@ public class Robot : Player
     public override void Start()
     {
         base.Start();
-        Instantiate(test, Vector3.zero, Quaternion.identity);
     }
 
     // Update is called once per frame
